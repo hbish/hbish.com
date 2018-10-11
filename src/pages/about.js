2ab54bb8aeedc9ef571abcb06e864afe10f05505
Ben Shi <ben@hbish.com>
2018-10-11 22:25:07 +1100

    about page fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,80 +3,83 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import { rhythm } from '../utils/typography'
+import styled from 'styled-components'
+import { SectionTitle } from '../components'
 
 class AboutPage extends React.Component {
   render() {
+    const Content = styled.article`
+      grid-column: 2;
+      box-shadow: 0 4px 120px rgba(0, 0, 0, 0.1);
+      max-width: 1000px;
+      border-radius: 1rem;
+      padding: 2rem 3rem;
+      background-color: 'white';
+    `
+
     return (
       <Layout location={this.props.location}>
-        <h1>About</h1>
-        <div class="entry">
-          <h2>Experience</h2>
-          <ul>
-            <li>
-              Microservice Engineer / Consultant @{' '}
-              <a href="https://versent.com.au">Versent Australia</a>
-            </li>
-            <li>
-              Senior Technology Associate @{' '}
+        <Content>
+          <SectionTitle>About</SectionTitle>
+          <div>
+            <h3>The gist</h3>
+            <p>
+              I'm a Software Engineer at{' '}
+              <a href="https://versent.com.au">Versent</a>, where we help
+              businesses with big hairy problems like migrating off traditional
+              software infrastructures and building out new API capabilities
+              using modern technology.
+            </p>
+            <p>
+              I was previously afilliated with{' '}
               <a href="https://www.jpmorgan.com/pages/jpmorgan">J.P Morgan</a>
-            </li>
-            <li>
-              Quality Assurance Engineer @{' '}
+              {', '}
               <a href="https://www.aciworldwide.com/">ACI Worldwide</a>
-            </li>
-            <li>
-              Test Engineer @{' '}
+              {', and '}
               <a href="https://www.crunchbase.com/company/distra">Distra</a>
-            </li>
-          </ul>
-          <h2>Education</h2>
-          <ul>
-            <li>
-              2008 - 2012: Bachelor of Engineering (Software), Diploma in
-              Engineering Practice @{' '}
-              <a href="https://www.uts.edu.au/">
-                University of Technology, Sydney
-              </a>
-            </li>
-            <li>
-              2012: Innovation, Technology and Entrepreneurship in China @{' '}
-              <a href="https://hku.hk">The University of Hong Kong</a>
-            </li>
-          </ul>
-          <h2>Special Achievements</h2>
-          <ul>
-            <li>
-              Winner of JPM Asia Pacific CEO Award (Team Category) 2015, 2016{' '}
-            </li>
-            <li>
-              Winner of Flow Monster Project, J.P. Morgan Technology Graduate
-              Training 2013
-            </li>
-            <li>
-              Winner of Telstra/Pollenizer Appiness Hackathon 2013 w/ Poppy [
-              <a href="https://exchange.telstra.com.au/2013/06/04/team-poppy-named-winner-of-inaugural-appiness-hackathon/">
-                article
-              </a>
-              , <a href="https://youtu.be/sV8BRsQ0m8g">pitch</a>]
-            </li>
-          </ul>
-          <h2>Interests</h2>
-          <ul>
-            <li>Web technology</li>
-            <li>Financial services</li>
-            <li>Mobile technology</li>
-            <li>Big data</li>
-            <li>Data visualisation</li>
-            <li>Blockchain &amp; Cryptocurrency</li>
-          </ul>
-          <h2>Hobbies</h2>
-          <ul>
-            <li>Photography</li>
-            <li>Reading</li>
-            <li>Hiking</li>
-            <li>Travelling</li>
-          </ul>
-        </div>
+            </p>
+
+            <h3>An unlucky start</h3>
+            <p>
+              I enjoy coding but it wasn't always what I wanted to do. Growing
+              up I always wanted to be a mechanic, I loved solving problems and
+              taking things apart then putting it back together. But one
+              unfortunate event early on in my life changed all that.
+            </p>
+            <p>
+              I'm a right hander. One summer afternoon in 2002, I had an
+              accident that severely damage my right arm. It took over 10 hours
+              of surgery and more than 100 stitches to just put me back in one
+              piece. In the following years, I got 2 additional follow up
+              surgeries to help me with my sensations in my arm and fingers. The
+              recovery process was hard but for me relearning everything with my
+              left hand was harder both mentally and physically. It is at this
+              stage in my life I realise that I was probably never going to be a
+              mechanic...
+            </p>
+            <p>
+              It is around this time, my mother got me my first computer, a
+              Compaq desktop running on Windows ME (yuk I know) to help with my
+              homework. The more I used it the more I became intrigued about how
+              everything worked, from webpages, email and the awful dial-tone we
+              had to listen to just to connect to the WWW.
+            </p>
+            <p>
+              I learned Dreamweaver with some HTML, built my very first website
+              on GeoCities and the rest was history. In many ways programming is
+              exactly like building and maintaining a car. Coding up a new
+              feature is like modifying a car to look or perform better. Finding
+              a bug is like disassembling a car and zeroing on the problem.
+            </p>
+            <p>
+              Till this day, I have not gotten 100% of sensation back and I
+              probably will never. But I am extremely blessed to have discovered
+              the fascinating world of technology and programming. There are not
+              many people out there can say they are able to do something they
+              enjoy day in and day out as a career.
+            </p>
+          </div>
+        </Content>
       </Layout>
     )
   }
