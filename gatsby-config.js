64e41907c616a8b6c0dfb429bef1f98f106c444b
Ben Shi <ben.shi@versent.com.au>
2018-10-16 13:48:57 +1100

    fix pwa settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
         short_name: `hbish`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `##1ca086`,
+        theme_color: `#1ca086`,
         display: `minimal-ui`,
         icon: `static/favicon.png`,
       },
